d777a53c6fc38f3a6b0a2153b4b7edb9545cda6d
Alex Sawyer <alexsawyer95@hotmail.com>
2025-03-31 21:45:39 +0100

    Add GlucoPilot.Identity.

---
+++ src/GlucoPilot.Identity/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GlucoPilot.Identity
+{
+    public static class Startup
+    {
+        public static IServiceCollection AddIdentity(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddApiVersioning();
+            services.AddProblemDetails();
+
+            return services;
+        }
+        
+    }
+}
