ce3d3506470d3aaa5bcbd5761da3d3be841fb3cc
Joseph Sawyer <joseph@jcsawyer.com>
2025-04-01 07:10:18 +0100

    feat: add and implement identity token service

---
+++ src/GlucoPilot.Identity/Services/TokenService.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using GlucoPilot.Data.Entities;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace GlucoPilot.Identity.Services;
+
+internal sealed class TokenService
+{
+    private readonly IdentityOptions _options;
+    
+    public TokenService(IOptions<IdentityOptions> options)
+    {
+        _options = options.Value ?? throw new ArgumentNullException(nameof(options));
+    }
+    
+    public string GenerateJwtToken(User user)
+    {
+        ArgumentNullException.ThrowIfNull(user);
+        
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(_options.TokenSigningKey);
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(GetClaims(user)),
+            Expires = DateTime.UtcNow.AddMinutes(_options.TokenExpirationInMinutes),
+            SigningCredentials =
+                new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
+        };
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return tokenHandler.WriteToken(token);
+    }
+
+    private static IEnumerable<Claim> GetClaims(User user)
+    {
+        yield return new Claim(ClaimTypes.NameIdentifier, user.Id.ToString());
+        yield return new Claim(ClaimTypes.Email, user.Email);
+    }
+}
