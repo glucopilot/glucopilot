1be281214a66f914a5c4c0ecf9448b9d5204d31c
Joseph Sawyer <joseph@jcsawyer.com>
2025-04-02 19:39:50 +0100

    refactor: exclude data startup from code coverage (#45)

--- src/GlucoPilot.Data/Startup.cs
+++ src/GlucoPilot.Data/Startup.cs
@@ -3,42 +3,43 @@ using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using System;
+using System.Diagnostics.CodeAnalysis;
 
-namespace GlucoPilot.Data
+namespace GlucoPilot.Data;
+
+[ExcludeFromCodeCoverage]
+public static class Startup
 {
-    public static class Startup
+    public static IServiceCollection AddData(this IServiceCollection services, Action<DatabaseOptions> configure)
     {
-        public static IServiceCollection AddData(this IServiceCollection services, Action<DatabaseOptions> configure)
-        {
-            services.AddOptions<DatabaseOptions>()
-                .Configure(configure)
-                .ValidateDataAnnotations()
-                .ValidateOnStart();
+        services.AddOptions<DatabaseOptions>()
+            .Configure(configure)
+            .ValidateDataAnnotations()
+            .ValidateOnStart();
 
 
-            return services
-                .AddDbContext<GlucoPilotDbContext>((provider, options) =>
-                {
-                    var dbOptions = provider.GetRequiredService<IOptions<DatabaseOptions>>().Value;
-
-                    options.UseDatabase(dbOptions.DbProvider, dbOptions.ConnectionString);
-#if DEBUG
-                    options.ConfigureWarnings(warnings => warnings.Throw(RelationalEventId.MultipleCollectionIncludeWarning));
-#endif
-                });
-        }
-
-        public static DbContextOptionsBuilder UseDatabase(this DbContextOptionsBuilder builder, string dbProvider,
-            string connectionString)
-        {
-            switch (dbProvider.ToLowerInvariant())
+        return services
+            .AddDbContext<GlucoPilotDbContext>((provider, options) =>
             {
-                case DatabaseOptions.DatabaseProviderKeys.SqlServer:
-                    return builder.UseSqlServer(connectionString,
-                        e => e.MigrationsAssembly("GlucoPilot.Data.Migrators.MSSQL"));
-                default:
-                    throw new InvalidOperationException($"DB provider '{dbProvider}' is not supported.");
-            }
+                var dbOptions = provider.GetRequiredService<IOptions<DatabaseOptions>>().Value;
+
+                options.UseDatabase(dbOptions.DbProvider, dbOptions.ConnectionString);
+#if DEBUG
+                options.ConfigureWarnings(warnings => warnings.Throw(RelationalEventId.MultipleCollectionIncludeWarning));
+#endif
+            });
+    }
+
+    public static DbContextOptionsBuilder UseDatabase(this DbContextOptionsBuilder builder, string dbProvider,
+        string connectionString)
+    {
+        switch (dbProvider.ToLowerInvariant())
+        {
+            case DatabaseOptions.DatabaseProviderKeys.SqlServer:
+                return builder.UseSqlServer(connectionString,
+                    e => e.MigrationsAssembly("GlucoPilot.Data.Migrators.MSSQL"));
+            default:
+                throw new InvalidOperationException($"DB provider '{dbProvider}' is not supported.");
         }
     }
 }
