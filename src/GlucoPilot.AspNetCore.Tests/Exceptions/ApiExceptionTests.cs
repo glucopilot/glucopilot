a997c5e73545e4fcdd7d694bbfd7a8685a3646e9
Joseph Sawyer <joseph@jcsawyer.com>
2025-04-02 18:08:01 +0100

    test: add api exception tests

---
+++ src/GlucoPilot.AspNetCore.Tests/Exceptions/ApiExceptionTests.cs
@@ -0,0 +1,48 @@
+using System.Net;
+using GlucoPilot.AspNetCore.Exceptions;
+
+namespace GlucoPilot.AspNetCore.Tests.Exceptions;
+
+[TestFixture]
+internal sealed class ApiExceptionTests
+{
+    [Test]
+    public void ConflictException_Should_Set_Properties_Correctly()
+    {
+        var message = "Conflict occurred";
+        var exception = new ConflictException(message);
+
+        Assert.That(exception.Message, Is.EqualTo(message));
+        Assert.That(exception.StatusCode, Is.EqualTo(HttpStatusCode.Conflict));
+    }
+    
+    [Test]
+    public void ForbiddenException_Should_Set_Properties_Correctly()
+    {
+        var message = "Forbidden access";
+        var exception = new ForbiddenException(message);
+
+        Assert.That(exception.Message, Is.EqualTo(message));
+        Assert.That(exception.StatusCode, Is.EqualTo(HttpStatusCode.Forbidden));
+    }
+    
+    [Test]
+    public void NotFoundException_Should_Set_Properties_Correctly()
+    {
+        var message = "Resource not found";
+        var exception = new NotFoundException(message);
+
+        Assert.That(exception.Message, Is.EqualTo(message));
+        Assert.That(exception.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
+    }
+    
+    [Test]
+    public void UnauthorizedException_Should_Set_Properties_Correctly()
+    {
+        var message = "Unauthorized access";
+        var exception = new UnauthorizedException(message);
+
+        Assert.That(exception.Message, Is.EqualTo(message));
+        Assert.That(exception.StatusCode, Is.EqualTo(HttpStatusCode.Unauthorized));
+    }
+}
