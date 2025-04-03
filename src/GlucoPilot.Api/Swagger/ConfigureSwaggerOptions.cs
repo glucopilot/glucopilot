68af69e573baa1eac9c50cb616ec806a65699b09
jcsawyer <8984041+jcsawyer@users.noreply.github.com>
2025-04-03 20:27:20 +0000

    Run code generators and linters

--- src/GlucoPilot.Api/Swagger/ConfigureSwaggerOptions.cs
+++ src/GlucoPilot.Api/Swagger/ConfigureSwaggerOptions.cs
@@ -23,43 +23,43 @@ internal sealed class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOpti
     /// Initializes a new instance of the <see cref="ConfigureSwaggerOptions"/> class.
     /// </summary>
     /// <param name="provider">The <see cref="IApiVersionDescriptionProvider">provider</see> used to generate Swagger documents.</param>
-    public ConfigureSwaggerOptions( IApiVersionDescriptionProvider provider ) => this.provider = provider;
+    public ConfigureSwaggerOptions(IApiVersionDescriptionProvider provider) => this.provider = provider;
 
     /// <inheritdoc />
-    public void Configure( SwaggerGenOptions options )
+    public void Configure(SwaggerGenOptions options)
     {
         // add a swagger document for each discovered API version
         // note: you might choose to skip or document deprecated API versions differently
-        foreach ( var description in provider.ApiVersionDescriptions )
+        foreach (var description in provider.ApiVersionDescriptions)
         {
-            options.SwaggerDoc( description.GroupName, CreateInfoForApiVersion( description ) );
+            options.SwaggerDoc(description.GroupName, CreateInfoForApiVersion(description));
         }
     }
 
-    private static OpenApiInfo CreateInfoForApiVersion( ApiVersionDescription description )
+    private static OpenApiInfo CreateInfoForApiVersion(ApiVersionDescription description)
     {
-        var text = new StringBuilder( "GlucoPilot API open API documentation." );
+        var text = new StringBuilder("GlucoPilot API open API documentation.");
         var info = new OpenApiInfo()
         {
             Title = "GlucoPilot API",
             Version = description.ApiVersion.ToString(),
         };
 
-        if ( description.IsDeprecated )
+        if (description.IsDeprecated)
         {
-            text.Append( " This API version has been deprecated." );
+            text.Append(" This API version has been deprecated.");
         }
 
-        if ( description.SunsetPolicy is { } policy )
+        if (description.SunsetPolicy is { } policy)
         {
-            if ( policy.Date is { } when )
+            if (policy.Date is { } when)
             {
-                text.Append( " The API will be sunset on " )
-                    .Append( when.Date.ToShortDateString() )
-                    .Append( '.' );
+                text.Append(" The API will be sunset on ")
+                    .Append(when.Date.ToShortDateString())
+                    .Append('.');
             }
 
-            if ( policy.HasLinks )
+            if (policy.HasLinks)
             {
                 text.AppendLine();
 
@@ -67,33 +67,33 @@ internal sealed class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOpti
 
                 foreach (var link in policy.Links)
                 {
-                    if ( link.Type == "text/html" )
+                    if (link.Type == "text/html")
                     {
-                        if ( !rendered )
+                        if (!rendered)
                         {
-                            text.Append( "<h4>Links</h4><ul>" );
+                            text.Append("<h4>Links</h4><ul>");
                             rendered = true;
                         }
 
-                        text.Append( "<li><a href=\"" );
-                        text.Append( link.LinkTarget.OriginalString );
-                        text.Append( "\">" );
+                        text.Append("<li><a href=\"");
+                        text.Append(link.LinkTarget.OriginalString);
+                        text.Append("\">");
                         text.Append(
-                            StringSegment.IsNullOrEmpty( link.Title )
+                            StringSegment.IsNullOrEmpty(link.Title)
                                 ? link.LinkTarget.OriginalString
-                                : link.Title.ToString() );
-                        text.Append( "</a></li>" );
+                                : link.Title.ToString());
+                        text.Append("</a></li>");
                     }
                 }
 
-                if ( rendered )
+                if (rendered)
                 {
-                    text.Append( "</ul>" );
+                    text.Append("</ul>");
                 }
             }
         }
 
-        text.Append( "<h4>Additional Information</h4>" );
+        text.Append("<h4>Additional Information</h4>");
         info.Description = text.ToString();
 
         return info;
