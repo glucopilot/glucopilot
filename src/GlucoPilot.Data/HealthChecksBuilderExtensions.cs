8ee76537c214a0514782d6dec5979048aab66b80
Alex Sawyer <alexsawyer95@hotmail.com>
2025-04-03 10:00:25 +0100

    feat: Add database health checks (#50)
    * Add health checks.
    
    * Use IHealthChecksBuilder extension
    
    * Add health checks.
    
    * Fix
    
    * Add health checks.
    
    Use IHealthChecksBuilder extension

---
+++ src/GlucoPilot.Data/HealthChecksBuilderExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace GlucoPilot.Data
+{
+    public static class HealthChecksBuilderExtensions
+    {
+        public static IHealthChecksBuilder AddDatabaseHealthChecks(this IHealthChecksBuilder builder)
+        {
+            return builder.AddDbContextCheck<GlucoPilotDbContext>("GlucoPilot-Database");
+        }
+    }
+}
