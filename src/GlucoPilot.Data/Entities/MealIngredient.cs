4759213f7a41a9e27c3351abc8d05ac78fe39414
Alex Sawyer <alexsawyer95@hotmail.com>
2025-04-04 22:07:30 +0100

    Add meal and ingredient ids to meal ingredient.

--- src/GlucoPilot.Data/Entities/MealIngredient.cs
+++ src/GlucoPilot.Data/Entities/MealIngredient.cs
@@ -10,17 +10,27 @@ public class MealIngredient
     /// <summary>
     /// The unique identifier for the meal ingredient.
     /// </summary>
-    public required Guid MealIngredientId { get; set; }
+    public required Guid Id { get; set; }
 
+    /// <summary>
+    /// the unique identifier for the meal.
+    /// </summary>
+    public required Guid MealId { get; set; }
+    
     /// <summary>
     /// The meals the meal ingredient is associated with.
     /// </summary>
-    public virtual required Meal Meal { get; set; }
+    public virtual Meal? Meal { get; set; }
+
+    /// <summary>
+    /// The unique identifier for the ingredient.
+    /// </summary>
+    public required Guid IngredientId { get; set; }
 
     /// <summary>
     /// The ingredients the meal ingredient is associated with.
     /// </summary>
-    public virtual required Ingredient Ingredient { get; set; }
+    public virtual Ingredient? Ingredient { get; set; }
 
     /// <summary>
     /// The quantity of the ingredient in the meal.
