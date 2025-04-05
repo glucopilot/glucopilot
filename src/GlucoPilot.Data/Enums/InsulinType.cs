69d8161703e2122cb75d1bb1321c23052ee6869a
Alex Sawyer <alexsawyer95@hotmail.com>
2025-04-05 13:57:10 +0100

    Comments for insulin types.

--- src/GlucoPilot.Data/Enums/InsulinType.cs
+++ src/GlucoPilot.Data/Enums/InsulinType.cs
@@ -2,6 +2,13 @@
 
 public enum InsulinType
 {
+    /// <summary>
+    /// A fast acting insulin, usually taken with a meal.
+    /// </summary>
     Bolus = 0,
+
+    /// <summary>
+    /// A long acting insulin, usually taken once or twice a day.
+    /// </summary>
     Basal = 1,
 }
