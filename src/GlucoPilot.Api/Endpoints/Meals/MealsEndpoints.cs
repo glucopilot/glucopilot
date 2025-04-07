08da1a9fc9f865af6d66e1fecc0775cd1e567146
Alex Sawyer <alexsawyer95@hotmail.com>
2025-04-07 22:21:02 +0100

    Map endpoint.

---
+++ src/GlucoPilot.Api/Endpoints/Meals/MealsEndpoints.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using System.Diagnostics.CodeAnalysis;
+
+namespace GlucoPilot.Api.Endpoints.Meals;
+
+[ExcludeFromCodeCoverage]
+public static class MealsEndpoints
+{
+    internal static IEndpointRouteBuilder MapMealsEndpoints(this IEndpointRouteBuilder endpoints)
+    {
+        var group = endpoints.NewVersionedApi().MapGroup("api/v{version:apiVersion}/meals")
+            .WithTags("Meals");
+        
+        group.MapGet("/", List.HandleAsync)
+            .HasApiVersion(1.0)
+            .RequireAuthorization();
+
+        return endpoints;
+    }
+}
