f8d0696365950383c301bcd4f76e76e1076742f7
Joseph Sawyer <joseph@jcsawyer.com>
2025-04-09 10:52:11 +0100

    feat: make llu client product/version configurable (#78)

--- src/GlucoPilot.LibreLinkClient/LibreLinkOptions.cs
+++ src/GlucoPilot.LibreLinkClient/LibreLinkOptions.cs
@@ -2,9 +2,8 @@ namespace GlucoPilot.LibreLinkClient;
 
 public sealed class LibreLinkOptions
 {
-    internal const string LinkUpVersion = "4.7.0";
-    internal const string LinkUpProduct = "llu.ios";
-
+    public string Version { get; set; } = "4.7.0";
+    public string Product { get; set; } = "llu.ios";
     public LibreRegion Region { get; set; }
     public string UserAgent { get; set; } = "Mozilla/5.0 (iPhone; CPU iPhone OS 16_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.5 Mobile/15E148 Safari/604.1";
 
