bcc8ad577221e1de30da558f01f17c636d668fba
Alex Sawyer <alexsawyer95@hotmail.com>
2025-04-10 18:48:47 +0100

    Add meal endpoint.

---
+++ src/GlucoPilot.Api.Tests/Endpoints/Meals/NewMealTests.cs
@@ -0,0 +1,65 @@
+﻿using NUnit.Framework;
+using Moq;
+using GlucoPilot.Api.Endpoints.Meals.NewMeal;
+using GlucoPilot.Data.Entities;
+using GlucoPilot.Data.Repository;
+using GlucoPilot.Identity.Authentication;
+using Microsoft.AspNetCore.Http.HttpResults;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using GlucoPilot.AspNetCore.Exceptions;
+using Microsoft.EntityFrameworkCore;
+
+[TestFixture]
+public class Endpoint_Tests
+{
+    private Mock<ICurrentUser> _currentUserMock;
+    private Mock<IRepository<Meal>> _repositoryMock;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _currentUserMock = new Mock<ICurrentUser>();
+        _repositoryMock = new Mock<IRepository<Meal>>();
+    }
+
+    [Test]
+    public void HandleAsync_Should_Throw_UnauthorizedException_When_User_Not_Logged_In()
+    {
+        _currentUserMock.Setup(x => x.GetUserId()).Returns((Guid?)null);
+        var request = new NewMealRequest { Name = "Test Meal", MealIngredients = new List<MealIngredient>() };
+
+        Assert.That(async () => await Endpoint.HandleAsync(request, _currentUserMock.Object, _repositoryMock.Object),
+            Throws.TypeOf<UnauthorizedException>().With.Message.EqualTo("USER_NOT_LOGGED_IN"));
+    }
+
+    [Test]
+    public async Task HandleAsync_Should_Return_Created_When_Meal_Is_Successfully_Created()
+    {
+        var userId = Guid.NewGuid();
+        _currentUserMock.Setup(x => x.GetUserId()).Returns(userId);
+        var request = new NewMealRequest { Name = "Test Meal", MealIngredients = new List<MealIngredient>() };
+
+        _repositoryMock.Setup(x => x.Add(It.IsAny<Meal>()));
+
+        var result = await Endpoint.HandleAsync(request, _currentUserMock.Object, _repositoryMock.Object);
+
+        Assert.That(result.Result, Is.InstanceOf<Created<NewMealResponse>>());
+        var createdResult = result.Result as Created<NewMealResponse>;
+        Assert.That(createdResult?.Value.Name, Is.EqualTo("Test Meal"));
+    }
+
+    [Test]
+    public void HandleAsync_Should_Throw_ConflictException_When_Meal_Already_Exists()
+    {
+        var userId = Guid.NewGuid();
+        _currentUserMock.Setup(x => x.GetUserId()).Returns(userId);
+        var request = new NewMealRequest { Name = "Test Meal", MealIngredients = new List<MealIngredient>() };
+
+        _repositoryMock.Setup(x => x.Add(It.IsAny<Meal>())).Throws(new DbUpdateException());
+
+        Assert.That(async () => await Endpoint.HandleAsync(request, _currentUserMock.Object, _repositoryMock.Object),
+            Throws.TypeOf<ConflictException>().With.Message.EqualTo("MEAL_ALREADY_EXISTS"));
+    }
+}
---
+++ src/GlucoPilot.Api/Endpoints/Meals/NewMeal/Endpoint.cs
@@ -0,0 +1,51 @@
+﻿using GlucoPilot.AspNetCore.Exceptions;
+using GlucoPilot.Data.Entities;
+using GlucoPilot.Data.Repository;
+using GlucoPilot.Identity.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+
+namespace GlucoPilot.Api.Endpoints.Meals.NewMeal;
+
+internal static class Endpoint
+{
+    internal static async Task<Results<Created<NewMealResponse>, Conflict, UnauthorizedHttpResult>> HandleAsync(
+        [FromBody] NewMealRequest request,
+        [FromServices] ICurrentUser currentUser,
+        [FromServices] IRepository<Meal> repository)
+    {
+        if (currentUser.GetUserId() is null)
+        {
+            throw new UnauthorizedException("USER_NOT_LOGGED_IN");
+        }
+
+        try
+        {
+            var newMeal = new Meal
+            {
+                Name = request.Name,
+                UserId = currentUser.GetUserId()!.Value,
+                Created = DateTimeOffset.UtcNow,
+                MealIngredients = request.MealIngredients,
+            };
+            repository.Add(newMeal);
+
+            var response = new NewMealResponse
+            {
+                Id = newMeal.Id,
+                Name = newMeal.Name,
+            };
+
+            return TypedResults.Created($"/meals/{newMeal.Id}", response);
+
+        }
+        catch (DbUpdateException)
+        {
+            throw new ConflictException("MEAL_ALREADY_EXISTS");
+        }     
+    }
+}
