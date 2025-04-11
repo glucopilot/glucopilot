a82521f95c3c7375918fcfd9b325f609887d17fe
Alex Sawyer <alexsawyer95@hotmail.com>
2025-04-11 22:58:51 +0100

    Add new ingredient endpoint

---
+++ src/GlucoPilot.Api/Endpoints/Ingredients/NewIngredient/Endpoint.cs
@@ -0,0 +1,59 @@
+﻿using FluentValidation;
+using GlucoPilot.Data.Entities;
+using GlucoPilot.Data.Repository;
+using GlucoPilot.Identity.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GlucoPilot.Api.Endpoints.Ingredients.NewIngredient;
+
+internal static class Endpoint
+{
+    internal static async Task<Results<Ok<NewIngredientResponse>, ValidationProblem>> HandleAsync(
+        [FromBody] NewIngredientRequest request,
+        [FromServices] IValidator<NewIngredientRequest> validator,
+        [FromServices] ICurrentUser currentUser,
+        [FromServices] IRepository<Ingredient> ingredientRepository,
+        CancellationToken cancellationToken)
+    {
+        if (await validator.ValidateAsync(request, cancellationToken).ConfigureAwait(false) is
+            { IsValid: false } validation)
+        {
+            return TypedResults.ValidationProblem(validation.ToDictionary());
+        }
+
+        var userId = currentUser.GetUserId();
+
+        var ingredient = new Ingredient
+        {
+            Name = request.Name,
+            Created = DateTimeOffset.UtcNow,
+            Carbs = request.Carbs,
+            Protein = request.Protein,
+            Fat = request.Fat,
+            Calories = request.Calories,
+            Uom = request.Uom,
+            UserId = userId,
+        };
+
+        ingredientRepository.Add(ingredient);
+
+        var response = new NewIngredientResponse
+        {
+            Id = ingredient.Id,
+            Created = ingredient.Created,
+            Name = ingredient.Name,
+            Carbs = ingredient.Carbs,
+            Protein = ingredient.Protein,
+            Fat = ingredient.Fat,
+            Calories = ingredient.Calories,
+            Uom = ingredient.Uom,
+        };
+
+        return TypedResults.Ok(response);
+    }
+}
---
+++ src/GlucoPilot.Api/Endpoints/Ingredients/NewIngredient/NewIngredientRequest.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation;
+using GlucoPilot.Data.Enums;
+using System;
+
+namespace GlucoPilot.Api.Endpoints.Ingredients.NewIngredient;
+
+public record NewIngredientRequest
+{
+    public required string Name { get; set; }
+    public int Carbs { get; set; }
+    public int Protein { get; set; }
+    public int Fat { get; set; }
+    public int Calories { get; set; }
+    public required UnitOfMeasurement Uom { get; set; }
+
+    public sealed class NewIngredientRequestValidator : AbstractValidator<NewIngredientRequest>
+    {
+        public NewIngredientRequestValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .WithMessage("NAME_IS_REQUIRED");
+            RuleFor(x => x.Carbs)
+                .GreaterThan(0)
+                .WithMessage("CARBS_GEATER_THAN_ZERO");
+            RuleFor(x => x.Protein)
+                .GreaterThan(0)
+                .WithMessage("PROTEIN_GEATER_THAN_ZERO");
+            RuleFor(x => x.Fat)
+                .GreaterThan(0)
+                .WithMessage("FAT_GEATER_THAN_ZERO");
+            RuleFor(x => x.Calories)
+                .GreaterThan(0)
+                .WithMessage("CALORIES_GEATER_THAN_ZERO");
+        }
+    }
+}
