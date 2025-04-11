d25c299db379ac27af33d0544d34ded6c91a1fa5
Alex Sawyer <alexsawyer95@hotmail.com>
2025-04-11 21:35:45 +0100

    Add delete meal endpoint

---
+++ src/GlucoPilot.Api/Endpoints/Meals/RemoveMeal/Endpoint.cs
@@ -0,0 +1,35 @@
+﻿using GlucoPilot.AspNetCore.Exceptions;
+using GlucoPilot.Data.Entities;
+using GlucoPilot.Data.Repository;
+using GlucoPilot.Identity.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GlucoPilot.Api.Endpoints.Meals.RemoveMeal;
+
+internal static class Endpoint
+{
+    internal static async Task<Results<NoContent, NotFound>> HandleAsync(
+        [FromQuery] Guid mealId,
+        [FromServices] IRepository<Meal> mealRepository,
+        [FromServices] ICurrentUser currentUser,
+        CancellationToken cancellationToken)
+    {
+        var userId = currentUser.GetUserId();
+
+        var meal = await mealRepository.FindOneAsync(m => m.Id == mealId && m.UserId == userId, new FindOptions { IsAsNoTracking = true }, cancellationToken);
+        
+        if (meal is null)
+        {
+            throw new NotFoundException("MEAL_NOT_FOUND");
+        }
+
+        await mealRepository.DeleteAsync(meal, cancellationToken);
+
+        return TypedResults.NoContent();
+    }
+}
