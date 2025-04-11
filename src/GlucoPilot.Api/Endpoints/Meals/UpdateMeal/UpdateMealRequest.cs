03911e8c98cbba06f9a778b17edcb613de1007ed
Alex Sawyer <alexsawyer95@hotmail.com>
2025-04-11 21:19:09 +0100

    feat: Add update meal endpoint. (#102)
    * Add update meal endpoint.
    
    * Fix PR comments.

---
+++ src/GlucoPilot.Api/Endpoints/Meals/UpdateMeal/UpdateMealRequest.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using System.Collections.Generic;
+using GlucoPilot.Api.Models;
+using System;
+
+namespace GlucoPilot.Api.Endpoints.Meals.UpdateMeal;
+
+public sealed record UpdateMealRequest
+{
+    public required Guid Id { get; set; }
+    public required string Name { get; set; }
+    public ICollection<NewMealIngredientRequest> MealIngredients { get; set; } = [];
+
+    public sealed class UpdateMealRequestValidator : AbstractValidator<UpdateMealRequest>
+    {
+        public UpdateMealRequestValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty();
+            RuleForEach(x => x.MealIngredients).SetValidator(new NewMealIngredientRequest.NewMealIngredientValidator());
+        }
+    }
+}
