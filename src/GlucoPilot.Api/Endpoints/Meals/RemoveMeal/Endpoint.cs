02163c6614e52770bddf8edf083a25aef19316a9
Alex Sawyer <alexsawyer95@hotmail.com>
2025-04-12 14:35:40 +0100

    Fix name

--- src/GlucoPilot.Api/Endpoints/Meals/RemoveMeal/Endpoint.cs
+++ src/GlucoPilot.Api/Endpoints/Meals/RemoveMeal/Endpoint.cs
@@ -14,14 +14,14 @@ namespace GlucoPilot.Api.Endpoints.Meals.RemoveMeal;
 internal static class Endpoint
 {
     internal static async Task<Results<NoContent, NotFound>> HandleAsync(
-        [FromRoute] Guid mealId,
+        [FromRoute] Guid id,
         [FromServices] IRepository<Meal> mealRepository,
         [FromServices] ICurrentUser currentUser,
         CancellationToken cancellationToken)
     {
         var userId = currentUser.GetUserId();
 
-        var meal = await mealRepository.FindOneAsync(m => m.Id == mealId && m.UserId == userId, new FindOptions { IsAsNoTracking = true }, cancellationToken);
+        var meal = await mealRepository.FindOneAsync(m => m.Id == id && m.UserId == userId, new FindOptions { IsAsNoTracking = true }, cancellationToken);
 
         if (meal is null)
         {
