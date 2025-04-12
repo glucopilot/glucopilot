4c159e4005c5ad76887c9f6ad96676114d761e81
Joseph Sawyer <joseph@jcsawyer.com>
2025-04-12 16:13:37 +0100

    refactor validator and add unit tests

---
+++ src/GlucoPilot.Api.Tests/Validators/LibreLinkLoginRequestValidatorTests.cs
@@ -0,0 +1,40 @@
+using FluentValidation.TestHelper;
+using GlucoPilot.Api.Endpoints.LibreLink.Login;
+using NUnit.Framework;
+
+namespace GlucoPilot.Api.Tests.Validators;
+
+[TestFixture]
+internal sealed class LibreLinkLoginRequestValidatorTests
+{
+    private readonly LoginRequest.LoginRequestValidator _validator;
+    
+    public LibreLinkLoginRequestValidatorTests()
+    {
+        _validator = new LoginRequest.LoginRequestValidator();
+    }
+    
+    [Test]
+    public void Should_Have_Error_When_Username_Is_Empty()
+    {
+        var model = new LoginRequest { Username = string.Empty, Password = "password" };
+        var result = _validator.TestValidate(model);
+        result.ShouldHaveValidationErrorFor(x => x.Username);
+    }
+    
+    [Test]
+    public void Should_Have_Error_When_Username_Is_Invalid_Email()
+    {
+        var model = new LoginRequest { Username = "invalid-email", Password = "password" };
+        var result = _validator.TestValidate(model);
+        result.ShouldHaveValidationErrorFor(x => x.Username);
+    }
+
+    [Test]
+    public void Should_Have_Error_When_Password_Is_Empty()
+    {
+        var model = new LoginRequest { Username = "test@nomail.com", Password = "" };
+        var result = _validator.TestValidate(model);
+        result.ShouldHaveValidationErrorFor(x => x.Password);
+    }
+}
