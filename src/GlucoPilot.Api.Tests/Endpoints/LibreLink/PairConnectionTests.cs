763e6afc7814f8a7a01a0a27f8889ea16ce29f26
Joseph Sawyer <joseph@jcsawyer.com>
2025-04-13 17:48:44 +0100

    fix: throw unauthorized when auth ticket is null or white space and authenticate before request

--- src/GlucoPilot.Api.Tests/Endpoints/LibreLink/PairConnectionTests.cs
+++ src/GlucoPilot.Api.Tests/Endpoints/LibreLink/PairConnectionTests.cs
@@ -13,6 +13,7 @@ using Microsoft.AspNetCore.Http.HttpResults;
 using GlucoPilot.AspNetCore.Exceptions;
 using GlucoPilot.LibreLinkClient.Models;
 using System.Linq.Expressions;
+using AuthTicket = GlucoPilot.Data.Entities.AuthTicket;
 
 namespace GlucoPilot.Api.Tests.Endpoints.LibreLink.PairConnection
 {
@@ -36,21 +37,51 @@ namespace GlucoPilot.Api.Tests.Endpoints.LibreLink.PairConnection
         {
             var userId = Guid.NewGuid();
             var patientId = Guid.NewGuid();
-            var patient = new Patient { Id = userId, Email = "test@test.com", PasswordHash = "passwordhash" };
+            var patient = new Patient
+            {
+                Id = userId, Email = "test@test.com", PasswordHash = "passwordhash", AuthTicket = new AuthTicket
+                {
+                    Token = "libreToken", Duration = 1000, Expires = long.MaxValue
+                }
+            };
             var request = new PairConnectionRequest { PatientId = patientId };
 
             _currentUserMock.Setup(x => x.GetUserId()).Returns(userId);
-            _patientRepositoryMock.Setup(x => x.FindOne(It.IsAny<Expression<Func<Patient, bool>>>(), It.IsAny<FindOptions>()))
+            _patientRepositoryMock.Setup(x =>
+                    x.FindOne(It.IsAny<Expression<Func<Patient, bool>>>(), It.IsAny<FindOptions>()))
                 .Returns(patient);
             _libreLinkClientMock.Setup(x => x.GetConnectionsAsync(It.IsAny<CancellationToken>()))
-                .ReturnsAsync(new[] { new ConnectionData { PatientId = patientId, FirstName = "Firstname", LastName = "LastName" } });
+                .ReturnsAsync(new[]
+                    { new ConnectionData { PatientId = patientId, FirstName = "Firstname", LastName = "LastName" } });
 
-            var result = await Endpoint.HandleAsync(request, _currentUserMock.Object, _patientRepositoryMock.Object, _libreLinkClientMock.Object, CancellationToken.None);
+            var result = await Endpoint.HandleAsync(request, _currentUserMock.Object, _patientRepositoryMock.Object,
+                _libreLinkClientMock.Object, CancellationToken.None);
 
             var okResult = result.Result as Ok<PairConnectionResponse>;
             Assert.That(okResult.Value, Is.InstanceOf<PairConnectionResponse>());
             Assert.That(okResult!.Value.Id, Is.EqualTo(userId));
             Assert.That(okResult.Value.PatientId, Is.EqualTo(patientId));
+        }
+
+        [Test]
+        public void HandleAsync_Should_Throw_UnauthorizedException_When_Patient_AuthTicket_Not_Found()
+        {
+            var userId = Guid.NewGuid();
+            var patientId = Guid.NewGuid();
+            var patient = new Patient { Id = userId, Email = "test@test.com", PasswordHash = "passwordhash" };
+            var request = new PairConnectionRequest { PatientId = patientId };
+
+            _currentUserMock.Setup(x => x.GetUserId()).Returns(userId);
+            _patientRepositoryMock.Setup(x =>
+                    x.FindOne(It.IsAny<Expression<Func<Patient, bool>>>(), It.IsAny<FindOptions>()))
+                .Returns(patient);
+            _libreLinkClientMock.Setup(x => x.GetConnectionsAsync(It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new[]
+                    { new ConnectionData { PatientId = patientId, FirstName = "Firstname", LastName = "LastName" } });
+
+            Assert.ThrowsAsync<UnauthorizedException>(async () =>
+                await Endpoint.HandleAsync(request, _currentUserMock.Object, _patientRepositoryMock.Object,
+                    _libreLinkClientMock.Object, CancellationToken.None));
         }
 
         [Test]
@@ -60,28 +91,38 @@ namespace GlucoPilot.Api.Tests.Endpoints.LibreLink.PairConnection
             var request = new PairConnectionRequest { PatientId = Guid.NewGuid() };
 
             _currentUserMock.Setup(x => x.GetUserId()).Returns(userId);
-            _patientRepositoryMock.Setup(x => x.FindOne(It.IsAny<Expression<Func<Patient, bool>>>(), It.IsAny<FindOptions>()))
+            _patientRepositoryMock.Setup(x =>
+                    x.FindOne(It.IsAny<Expression<Func<Patient, bool>>>(), It.IsAny<FindOptions>()))
                 .Returns((Patient)null);
 
             Assert.ThrowsAsync<UnauthorizedException>(async () =>
-                await Endpoint.HandleAsync(request, _currentUserMock.Object, _patientRepositoryMock.Object, _libreLinkClientMock.Object, CancellationToken.None));
+                await Endpoint.HandleAsync(request, _currentUserMock.Object, _patientRepositoryMock.Object,
+                    _libreLinkClientMock.Object, CancellationToken.None));
         }
 
         [Test]
         public void HandleAsync_Should_Throw_NotFoundException_When_Connection_Not_Found()
         {
             var userId = Guid.NewGuid();
-            var patient = new Patient { Id = userId, Email = "test@test.com", PasswordHash = "passwordhash" };
+            var patient = new Patient
+            {
+                Id = userId, Email = "test@test.com", PasswordHash = "passwordhash", AuthTicket = new AuthTicket
+                {
+                    Token = "libreToken", Duration = 1000, Expires = long.MaxValue
+                }
+            };
             var request = new PairConnectionRequest { PatientId = Guid.NewGuid() };
 
             _currentUserMock.Setup(x => x.GetUserId()).Returns(userId);
-            _patientRepositoryMock.Setup(x => x.FindOne(It.IsAny<Expression<Func<Patient, bool>>>(), It.IsAny<FindOptions>()))
+            _patientRepositoryMock.Setup(x =>
+                    x.FindOne(It.IsAny<Expression<Func<Patient, bool>>>(), It.IsAny<FindOptions>()))
                 .Returns(patient);
             _libreLinkClientMock.Setup(x => x.GetConnectionsAsync(It.IsAny<CancellationToken>()))
                 .ReturnsAsync(Array.Empty<ConnectionData>());
 
             Assert.ThrowsAsync<NotFoundException>(async () =>
-                await Endpoint.HandleAsync(request, _currentUserMock.Object, _patientRepositoryMock.Object, _libreLinkClientMock.Object, CancellationToken.None));
+                await Endpoint.HandleAsync(request, _currentUserMock.Object, _patientRepositoryMock.Object,
+                    _libreLinkClientMock.Object, CancellationToken.None));
         }
 
         [Test]
@@ -92,13 +133,15 @@ namespace GlucoPilot.Api.Tests.Endpoints.LibreLink.PairConnection
             var request = new PairConnectionRequest { PatientId = Guid.NewGuid() };
 
             _currentUserMock.Setup(x => x.GetUserId()).Returns(userId);
-            _patientRepositoryMock.Setup(x => x.FindOne(It.IsAny<Expression<Func<Patient, bool>>>(), It.IsAny<FindOptions>()))
+            _patientRepositoryMock.Setup(x =>
+                    x.FindOne(It.IsAny<Expression<Func<Patient, bool>>>(), It.IsAny<FindOptions>()))
                 .Returns(patient);
             _libreLinkClientMock.Setup(x => x.GetConnectionsAsync(It.IsAny<CancellationToken>()))
                 .ThrowsAsync(new LibreLinkAuthenticationFailedException());
 
             Assert.ThrowsAsync<UnauthorizedException>(async () =>
-                await Endpoint.HandleAsync(request, _currentUserMock.Object, _patientRepositoryMock.Object, _libreLinkClientMock.Object, CancellationToken.None));
+                await Endpoint.HandleAsync(request, _currentUserMock.Object, _patientRepositoryMock.Object,
+                    _libreLinkClientMock.Object, CancellationToken.None));
         }
     }
 }
