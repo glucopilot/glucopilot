da6ac06823de5e05d4aea1f30641a9899148ef95
Loko3610 <16038076+Loko3610@users.noreply.github.com>
2025-04-13 17:28:55 +0000

    Run code generators and linters

--- src/GlucoPilot.Api/Endpoints/Insulins/UpdateInsulin/Endpoint.cs
+++ src/GlucoPilot.Api/Endpoints/Insulins/UpdateInsulin/Endpoint.cs
@@ -31,7 +31,7 @@ internal static class Endpoint
         var userId = currentUser.GetUserId();
 
         var insulin = await insulinRepository.FindOneAsync(i => i.Id == id && i.UserId == userId, new FindOptions() { IsAsNoTracking = false }).ConfigureAwait(false);
-        
+
         if (insulin is null)
         {
             throw new NotFoundException("INSULIN_NOT_FOUND");
