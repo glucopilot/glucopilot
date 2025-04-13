a971aaa287f10c16273fc80edee15dd57c66bea2
Alex Sawyer <alexsawyer95@hotmail.com>
2025-04-13 19:05:24 +0100

    Add remove insulin endpoint.

--- src/GlucoPilot.Api/Endpoints/Insulins/InsulinsEndpoints.cs
+++ src/GlucoPilot.Api/Endpoints/Insulins/InsulinsEndpoints.cs
@@ -34,6 +34,11 @@ public static class InsulinsEndpoints
             .Produces<ErrorResult>(StatusCodes.Status401Unauthorized)
             .RequireAuthorization();
 
+        group.MapDelete("/{id:guid}", RemoveInsulin.Endpoint.HandleAsync)
+            .HasApiVersion(1.0)
+            .Produces<ErrorResult>(StatusCodes.Status401Unauthorized)
+            .RequireAuthorization();
+
         return endpoints;
     }
 }
---
+++ src/GlucoPilot.Api/Endpoints/Insulins/RemoveInsulin/Endpoint.cs
@@ -0,0 +1,34 @@
+﻿using GlucoPilot.AspNetCore.Exceptions;
+using GlucoPilot.Data.Entities;
+using GlucoPilot.Data.Repository;
+using GlucoPilot.Identity.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GlucoPilot.Api.Endpoints.Insulins.RemoveInsulin;
+
+internal static class Endpoint
+{
+    internal static async Task<Results<NoContent, NotFound, UnauthorizedHttpResult>> HandleAsync(
+        [FromRoute] Guid id,
+        [FromServices] IRepository<Insulin> insulinRepository,
+        [FromServices] ICurrentUser currentUser,
+        CancellationToken cancellationToken)
+    {
+        var userId = currentUser.GetUserId();
+
+        var insulin = await insulinRepository.FindOneAsync(i => i.Id == id && i.UserId == userId, new FindOptions { IsAsNoTracking = true }, cancellationToken);
+        if (insulin is null)
+        {
+            throw new NotFoundException("INSULIN_NOT_FOUND");
+        }
+
+        await insulinRepository.DeleteAsync(insulin, cancellationToken);
+
+        return TypedResults.NoContent();
+    }
+}
