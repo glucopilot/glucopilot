760a7888c95ef738e2d29d4ec96907de121ca800
Alex Sawyer <alexsawyer95@hotmail.com>
2025-04-15 19:33:45 +0100

    Add get treatment endpoint.

---
+++ src/GlucoPilot.Api/Endpoints/Treatments/GetTreatment/Endpoint.cs
@@ -0,0 +1,73 @@
+﻿using GlucoPilot.AspNetCore.Exceptions;
+using GlucoPilot.Data.Entities;
+using GlucoPilot.Data.Repository;
+using GlucoPilot.Identity.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GlucoPilot.Api.Endpoints.Treatments.GetTreatment;
+
+internal static class Endpoint
+{
+    internal static async Task<Results<Ok<GetTreatmentResponse>, NotFound, UnauthorizedHttpResult>> HandleAsync(
+        [FromRoute] Guid id,
+        [FromServices] ICurrentUser currentUser,
+        [FromServices] IRepository<Treatment> treatmentRepository,
+        CancellationToken cancellationToken)
+    {
+        var userId = currentUser.GetUserId();
+
+        var treatment = treatmentRepository
+            .GetAll(new FindOptions { IsAsNoTracking = true })
+            .Where(t => t.Id == id && t.UserId == userId)
+            .Include(t => t.Meal)
+            .ThenInclude(m => m.MealIngredients)
+            .ThenInclude(mi => mi.Ingredient)
+            .Include(t => t.Injection)
+            .ThenInclude(i => i.Insulin)
+            .Include(t => t.Reading)
+            .FirstOrDefault();
+
+        if (treatment is null)
+        {
+            throw new NotFoundException("TREATMENT_NOT_FOUND");
+        }
+
+        return TypedResults.Ok(new GetTreatmentResponse
+        {
+            Id = treatment.Id,
+            Created = treatment.Created,
+            Type = treatment.Type,
+            Meal = treatment.Meal is not null
+            ? new GetTreatmentMealResponse
+            {
+                Id = treatment.Meal.Id,
+                Name = treatment.Meal.Name,
+                TotalCalories = treatment.Meal.MealIngredients.Sum(mi => mi.Ingredient is null ? 0 : mi.Ingredient.Calories * mi.Quantity),
+                TotalCarbs = treatment.Meal.MealIngredients.Sum(mi => mi.Ingredient is null ? 0 : mi.Ingredient.Carbs * mi.Quantity),
+                TotalProtein = treatment.Meal.MealIngredients.Sum(mi => mi.Ingredient is null ? 0 : mi.Ingredient.Protein * mi.Quantity),
+                TotalFat = treatment.Meal.MealIngredients.Sum(mi => mi.Ingredient is null ? 0 : mi.Ingredient.Fat * mi.Quantity),
+            } : null,
+            Injection = treatment.Injection is not null
+            ? new GetTreatmentInjectionResponse
+            {
+                Id = treatment.Injection.Id,
+                InsulinName = treatment.Injection.Insulin?.Name ?? "",
+                Units = treatment.Injection.Units,
+            } : null,
+            Reading = treatment.Reading is not null
+            ? new GetTreatmentReadingResponse
+            {
+                Id = treatment.Reading.Id,
+                GlucoseLevel = treatment.Reading.GlucoseLevel,
+            } : null,
+
+        });
+    }
+}
---
+++ src/GlucoPilot.Api/Endpoints/Treatments/TreatmentEndpoints.cs
@@ -0,0 +1,24 @@
+﻿using GlucoPilot.AspNetCore.Exceptions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using System.Diagnostics.CodeAnalysis;
+
+namespace GlucoPilot.Api.Endpoints.Treatments;
+
+[ExcludeFromCodeCoverage]
+public static class TreatmentsEndpoints
+{
+    internal static IEndpointRouteBuilder MapTreatmentsEndpoints(this IEndpointRouteBuilder endpoints)
+    {
+        var group = endpoints.NewVersionedApi().MapGroup("api/v{version:apiVersion}/treatments")
+            .WithTags("Treatments");
+
+        group.MapGet("/{id:guid}", GetTreatment.Endpoint.HandleAsync)
+            .HasApiVersion(1.0)
+            .Produces<ErrorResult>(StatusCodes.Status401Unauthorized)
+            .RequireAuthorization();
+
+        return endpoints;
+    }
+}
