e9724c5e43edcf0aaaf373bffb9149ac0c7279b3
Alex Sawyer <alexsawyer95@hotmail.com>
2025-04-16 22:27:42 +0100

    remove not usings.

--- src/GlucoPilot.Api.Tests/Endpoints/Treatments/NewTreatmentTests.cs
+++ src/GlucoPilot.Api.Tests/Endpoints/Treatments/NewTreatmentTests.cs
@@ -9,10 +9,7 @@ using Microsoft.AspNetCore.Http.HttpResults;
 using Moq;
 using NUnit.Framework;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
