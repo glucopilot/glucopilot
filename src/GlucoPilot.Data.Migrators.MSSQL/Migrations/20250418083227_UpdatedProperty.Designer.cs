e80e2b1da47726aa65787771a04b1bbb5cc33a31
Alex Sawyer <alexsawyer95@hotmail.com>
2025-04-18 09:32:59 +0100

    Add migration.

--- src/GlucoPilot.Data.Migrators.MSSQL/Migrations/20250418083227_UpdatedProperty.Designer.cs
+++ src/GlucoPilot.Data.Migrators.MSSQL/Migrations/20250418083227_UpdatedProperty.Designer.cs
@@ -4,6 +4,7 @@ using GlucoPilot.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GlucoPilot.Data.Migrators.MSSQL.Migrations
 {
     [DbContext(typeof(GlucoPilotDbContext))]
-    partial class GlucoPilotDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250418083227_UpdatedProperty")]
+    partial class UpdatedProperty
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
