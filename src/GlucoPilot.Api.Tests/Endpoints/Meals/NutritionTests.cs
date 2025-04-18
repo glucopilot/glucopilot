ae07d89a46bcc2eefde51210659937cc0c36caee
Joseph Sawyer <joseph@jcsawyer.com>
2025-04-19 00:14:32 +0100

    add missing coverage

--- src/GlucoPilot.Api.Tests/Endpoints/Meals/NutritionTests.cs
+++ src/GlucoPilot.Api.Tests/Endpoints/Meals/NutritionTests.cs
@@ -5,7 +5,9 @@ using System.Linq.Expressions;
 using System.Threading;
 using System.Threading.Tasks;
 using FluentValidation;
+using FluentValidation.Results;
 using GlucoPilot.Api.Endpoints.Meals.Nutrition;
+using GlucoPilot.AspNetCore.Exceptions;
 using GlucoPilot.Data.Entities;
 using GlucoPilot.Data.Enums;
 using GlucoPilot.Data.Repository;
@@ -13,6 +15,7 @@ using GlucoPilot.Identity.Authentication;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Moq;
 using NUnit.Framework;
+using Endpoint = GlucoPilot.Api.Endpoints.Meals.Nutrition.Endpoint;
 
 namespace GlucoPilot.Api.Tests.Endpoints.Meals
 {
@@ -35,8 +38,8 @@ namespace GlucoPilot.Api.Tests.Endpoints.Meals
         public async Task HandleAsync_Should_Return_ValidationProblem_When_Request_Is_Invalid()
         {
             var request = new NutritionRequestModel();
-            var validationResult = new FluentValidation.Results.ValidationResult([
-                new FluentValidation.Results.ValidationFailure("Property", "Error message")
+            var validationResult = new ValidationResult([
+                new ValidationFailure("Property", "Error message")
             ]);
 
             _validatorMock
@@ -46,6 +49,24 @@ namespace GlucoPilot.Api.Tests.Endpoints.Meals
             var result = await Endpoint.HandleAsync(request, _validatorMock.Object, _currentUserMock.Object, _repositoryMock.Object, CancellationToken.None);
 
             Assert.That(result.Result, Is.InstanceOf<ValidationProblem>());
+        }
+        
+        [Test]
+        public void HandleAsync_Returns_Unauthorized_When_User_Is_Not_Authenticated()
+        {
+            var mockValidator = new Mock<IValidator<NutritionRequestModel>>();
+            mockValidator
+                .Setup(v => v.ValidateAsync(It.IsAny<NutritionRequestModel>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new ValidationResult());
+
+            _currentUserMock.Setup(x => x.GetUserId()).Throws(new UnauthorizedException("USER_NOT_LOGGED_IN"));
+
+            var mockRepository = new Mock<IRepository<Meal>>();
+
+            var request = new NutritionRequestModel { From = DateTimeOffset.UtcNow.AddDays(-1), To = DateTimeOffset.UtcNow };
+
+            Assert.That(() => Endpoint.HandleAsync(request, _validatorMock.Object, _currentUserMock.Object, _repositoryMock.Object, CancellationToken.None),
+                Throws.InstanceOf<UnauthorizedException>().With.Message.EqualTo("USER_NOT_LOGGED_IN"));
         }
 
         [Test]
@@ -109,6 +130,50 @@ namespace GlucoPilot.Api.Tests.Endpoints.Meals
             Assert.That(response.TotalCarbs, Is.EqualTo(40));
             Assert.That(response.TotalProtein, Is.EqualTo(20));
             Assert.That(response.TotalFat, Is.EqualTo(10));
+        }
+        
+        [Test]
+        public async Task HandleAsync_Should_Ignore_Treatments_With_No_Meals()
+        {
+            var userId = Guid.NewGuid();
+            var request = new NutritionRequestModel { From = DateTimeOffset.UtcNow.AddDays(-1), To = DateTimeOffset.UtcNow };
+
+            _validatorMock
+                .Setup(v => v.ValidateAsync(request, It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new FluentValidation.Results.ValidationResult());
+
+            _currentUserMock
+                .Setup(c => c.GetUserId())
+                .Returns(userId);
+
+            var treatments = new List<Treatment>
+            {
+                new Treatment
+                {
+                    UserId = userId,
+                    Created = DateTimeOffset.UtcNow.AddMinutes(-1),
+                    Injection = new Injection()
+                    {
+                        Created = DateTimeOffset.UtcNow.AddMinutes(-1),
+                        InsulinId = Guid.NewGuid(),
+                        Units = 1,
+                    }
+                }
+            };
+
+            _repositoryMock
+                .Setup(r => r.Find(It.IsAny<Expression<Func<Treatment, bool>>>(), It.IsAny<FindOptions>()))
+                .Returns(treatments.AsQueryable());
+
+            var result = await Endpoint.HandleAsync(request, _validatorMock.Object, _currentUserMock.Object, _repositoryMock.Object, CancellationToken.None);
+
+            Assert.That(result.Result, Is.InstanceOf<Ok<NutritionResponseModel>>());
+            var response = (result.Result as Ok<NutritionResponseModel>)?.Value;
+            Assert.That(response, Is.Not.Null);
+            Assert.That(response.TotalCalories, Is.EqualTo(0));
+            Assert.That(response.TotalCarbs, Is.EqualTo(0));
+            Assert.That(response.TotalProtein, Is.EqualTo(0));
+            Assert.That(response.TotalFat, Is.EqualTo(0));
         }
     }
 }
