c9d5a12f60d4dee49e79ed760ca52b8192e368b7
Alex Sawyer <alexsawyer95@hotmail.com>
2025-04-18 09:42:58 +0100

    Add migration. (#149)

---
+++ src/GlucoPilot.Data.Migrators.MSSQL/Migrations/20250418083227_UpdatedProperty.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GlucoPilot.Data.Migrators.MSSQL.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "Updated",
+                table: "treatments",
+                type: "datetimeoffset",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "Updated",
+                table: "meals",
+                type: "datetimeoffset",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "Updated",
+                table: "insulin",
+                type: "datetimeoffset",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "Updated",
+                table: "injections",
+                type: "datetimeoffset",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "Updated",
+                table: "ingredients",
+                type: "datetimeoffset",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Updated",
+                table: "treatments");
+
+            migrationBuilder.DropColumn(
+                name: "Updated",
+                table: "meals");
+
+            migrationBuilder.DropColumn(
+                name: "Updated",
+                table: "insulin");
+
+            migrationBuilder.DropColumn(
+                name: "Updated",
+                table: "injections");
+
+            migrationBuilder.DropColumn(
+                name: "Updated",
+                table: "ingredients");
+        }
+    }
+}
