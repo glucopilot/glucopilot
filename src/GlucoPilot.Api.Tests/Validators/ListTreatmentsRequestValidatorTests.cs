cb89f5a16f647b4f8c52a542a67e34cb4bbdacbb
Alex Sawyer <alexsawyer95@hotmail.com>
2025-04-21 11:01:31 +0100

    feat: Add date range in list treatments request. (#164)
    * Add date range in list treatments request.
    
    * Run code generators and linters
    
    ---------
    
    Co-authored-by: Loko3610 <16038076+Loko3610@users.noreply.github.com>

---
+++ src/GlucoPilot.Api.Tests/Validators/ListTreatmentsRequestValidatorTests.cs
@@ -0,0 +1,92 @@
+﻿using FluentValidation.TestHelper;
+using GlucoPilot.Api.Endpoints.Treatments.ListTreatments;
+using GlucoPilot.Api.Models;
+using Microsoft.Extensions.Options;
+using NUnit.Framework;
+using System;
+
+namespace GlucoPilot.Api.Tests.Validators;
+
+[TestFixture]
+public class ListTreatmentsRequestValidatorTests
+{
+    private ListTreatmentsRequest.ListTreatmentsRequestValidator _validator;
+
+    [SetUp]
+    public void SetUp()
+    {
+        var apiSettings = Options.Create(new ApiSettings { MaxPageSize = 25 });
+        _validator = new ListTreatmentsRequest.ListTreatmentsRequestValidator(apiSettings);
+    }
+
+    [Test]
+    public void Should_Not_Have_Error_When_To_Is_Null()
+    {
+        var request = new ListTreatmentsRequest
+        {
+            From = DateTimeOffset.Now.AddDays(-1),
+            To = null
+        };
+
+        var result = _validator.TestValidate(request);
+
+        result.ShouldNotHaveValidationErrorFor(r => r.From);
+    }
+
+    [Test]
+    public void Should_Not_Have_Error_When_To_Is_Null_And_From_Is_In_Future()
+    {
+        var request = new ListTreatmentsRequest
+        {
+            From = DateTimeOffset.Now.AddDays(1),
+            To = null
+        };
+
+        var result = _validator.TestValidate(request);
+
+        result.ShouldNotHaveValidationErrorFor(r => r.From);
+    }
+
+    [Test]
+    public void Should_Have_Error_When_From_Is_After_To()
+    {
+        var request = new ListTreatmentsRequest
+        {
+            From = DateTimeOffset.Now.AddDays(-1),
+            To = DateTimeOffset.Now.AddDays(-2)
+        };
+
+        var result = _validator.TestValidate(request);
+
+        result.ShouldHaveValidationErrorFor(r => r)
+            .WithErrorMessage("TO_BEFORE_FROM");
+    }
+
+    [Test]
+    public void Should_Not_Have_Error_When_From_Is_Before_To()
+    {
+        var request = new ListTreatmentsRequest
+        {
+            From = DateTimeOffset.Now.AddDays(-2),
+            To = DateTimeOffset.Now.AddDays(-1)
+        };
+
+        var result = _validator.TestValidate(request);
+
+        result.ShouldNotHaveValidationErrorFor(r => r);
+    }
+
+    [Test]
+    public void Should_Not_Have_Error_When_Both_From_And_To_Are_Null()
+    {
+        var request = new ListTreatmentsRequest
+        {
+            From = null,
+            To = null
+        };
+
+        var result = _validator.TestValidate(request);
+
+        result.ShouldNotHaveValidationErrorFor(r => r);
+    }
+}
--- src/GlucoPilot.Api/Endpoints/Treatments/ListTreatments/ListTreatmentsRequest.cs
+++ src/GlucoPilot.Api/Endpoints/Treatments/ListTreatments/ListTreatmentsRequest.cs
@@ -1,14 +1,22 @@
+using FluentValidation;
 using GlucoPilot.Api.Models;
 using Microsoft.Extensions.Options;
+using System;
 
 namespace GlucoPilot.Api.Endpoints.Treatments.ListTreatments;
 
 public sealed record ListTreatmentsRequest : PagedRequest
 {
+    public DateTimeOffset? From { get; set; }
+    public DateTimeOffset? To { get; set; }
+
     public sealed class ListTreatmentsRequestValidator : PagedRequestValidator<ListTreatmentsRequest>
     {
         public ListTreatmentsRequestValidator(IOptions<ApiSettings> apiSettings) : base(apiSettings)
         {
+            RuleFor(request => request)
+                .Must(r => r.From == null || r.To == null || r.From <= r.To)
+                .WithMessage(Resources.ValidationMessages.ToBeforeFrom);
         }
     }
 }
