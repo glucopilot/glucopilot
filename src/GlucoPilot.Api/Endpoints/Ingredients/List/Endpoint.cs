20bd932bb0c09306e0f0111d6993b751a4a6fa39
Alex Sawyer <alexsawyer95@hotmail.com>
2025-05-07 20:26:41 +0100

    Add search to list ingredient endpoint

--- src/GlucoPilot.Api/Endpoints/Ingredients/List/Endpoint.cs
+++ src/GlucoPilot.Api/Endpoints/Ingredients/List/Endpoint.cs
@@ -29,23 +29,32 @@ internal static class Endpoint
 
         var userId = currentUser.GetUserId();
 
-        var ingredients = repository.Find(i => i.UserId == userId, new FindOptions { IsAsNoTracking = true })
-            .OrderByDescending(i => i.Created)
-            .Skip(request.Page * request.PageSize)
-            .Take(request.PageSize)
-            .Select(i => new GetIngredientResponse
-            {
-                Id = i.Id,
-                Created = i.Created,
-                Name = i.Name,
-                Carbs = i.Carbs,
-                Protein = i.Protein,
-                Fat = i.Fat,
-                Calories = i.Calories,
-                Uom = i.Uom,
-                Updated = i.Updated
-            })
-            .ToList();
+        var ingredientsQuery = repository.Find(i => i.UserId == userId, new FindOptions { IsAsNoTracking = true });
+
+        if (!string.IsNullOrWhiteSpace(request.Search))
+        {
+            var searchLower = request.Search.ToLowerInvariant();
+#pragma warning disable CA1862 // EF cannot use a query with a .Contains(StringComparison) within it.
+            ingredientsQuery = ingredientsQuery.Where(m => m.Name.ToLower().Contains(searchLower));
+#pragma warning restore CA1862 // EF cannot use a query with a .Contains(StringComparison) within it.
+        }
+
+        var ingredients = ingredientsQuery.OrderByDescending(i => i.Created)
+        .Skip(request.Page * request.PageSize)
+        .Take(request.PageSize)
+        .Select(i => new GetIngredientResponse
+        {
+            Id = i.Id,
+            Created = i.Created,
+            Name = i.Name,
+            Carbs = i.Carbs,
+            Protein = i.Protein,
+            Fat = i.Fat,
+            Calories = i.Calories,
+            Uom = i.Uom,
+            Updated = i.Updated
+        })
+        .ToList();
 
         var totalMeals = await repository.CountAsync(i => i.UserId == userId, cancellationToken).ConfigureAwait(false);
         var response = new ListIngredientsResponse
--- src/GlucoPilot.Api/Endpoints/Ingredients/List/ListIngredientsRequest.cs
+++ src/GlucoPilot.Api/Endpoints/Ingredients/List/ListIngredientsRequest.cs
@@ -1,14 +1,20 @@
-﻿using GlucoPilot.Api.Models;
+﻿using FluentValidation;
+using GlucoPilot.Api.Models;
 using Microsoft.Extensions.Options;
 
 namespace GlucoPilot.Api.Endpoints.Ingredients.List;
 
 public sealed record ListIngredientsRequest : PagedRequest
 {
+    public string? Search { get; set; } = null;
     public sealed class ListIngredientsValidator : PagedRequestValidator<ListIngredientsRequest>
-    {
+    {     
         public ListIngredientsValidator(IOptions<ApiSettings> apiSettings) : base(apiSettings)
         {
+            RuleFor(x => x.Search)
+            .MinimumLength(3)
+            .When(x => !string.IsNullOrEmpty(x.Search))
+            .WithMessage(Resources.ValidationMessages.SearchLengthInvalid);
         }
     }
 }
