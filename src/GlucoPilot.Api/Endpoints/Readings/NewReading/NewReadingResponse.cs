5f4ffb14cd6f69792999ce5a9784f843796564c0
Alex Sawyer <alexsawyer95@hotmail.com>
2025-05-28 21:18:13 +0100

    Add new reading response to new reading endpoint

---
+++ src/GlucoPilot.Api/Endpoints/Readings/NewReading/NewReadingResponse.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace GlucoPilot.Api.Endpoints.Readings.NewReading;
+
+public sealed record NewReadingResponse
+{
+    public Guid Id { get; set; }
+    public DateTimeOffset Created { get; set; }
+    public double GlucoseLevel { get; set; }
+}
