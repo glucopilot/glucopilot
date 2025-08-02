6b8398c6d430a565b582b9f5043339b0e8818655
Loko3610 <alexsawyer95@hotmail.com>
2025-08-02 20:25:16 +0100

    Add test coverage.

--- src/GlucoPilot.Nutrition/NutritionOptions.cs
+++ src/GlucoPilot.Nutrition/NutritionOptions.cs
@@ -1,7 +1,9 @@
 using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
 
 namespace GlucoPilot.Nutrition;
 
+[ExcludeFromCodeCoverage]
 public sealed class NutritionOptions
 {
     [Required] public string ConnectionString { get; init; } = "";
