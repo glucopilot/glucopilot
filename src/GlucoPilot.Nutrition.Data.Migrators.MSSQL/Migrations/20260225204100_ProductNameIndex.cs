029f4fd93d040a77a0b8b6a569d04d0c4b5bc8d5
Joseph Sawyer <joseph@jcsawyer.com>
2026-02-26 18:57:55 +0000

    feat: create nutrition product name full text index (#263)
    * feat: create nutrition product name full text index
    
    * refactor: simplify sql script reading
    
    * remove scripts output

---
+++ src/GlucoPilot.Nutrition.Data.Migrators.MSSQL/Migrations/20260225204100_ProductNameIndex.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GlucoPilot.Nutrition.Data.Migrators.MSSQL.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProductNameIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sql = """
+                      CREATE FULLTEXT CATALOG FTCProducts AS DEFAULT;
+
+                      CREATE FULLTEXT INDEX ON dbo.Products(ProductName)
+                        KEY INDEX PK_Products ON FTCProducts
+                        WITH STOPLIST = OFF, CHANGE_TRACKING AUTO;
+                      """;
+            migrationBuilder.Sql(sql, true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            var sql = """
+                      DROP FULLTEXT INDEX on dbo.Products;
+                      DROP FULLTEXT CATALOG FTCProducts;
+                      """;
+            migrationBuilder.Sql(sql, true);
+        }
+    }
+}
