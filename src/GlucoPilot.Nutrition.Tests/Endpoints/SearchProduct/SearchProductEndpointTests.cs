ee09a28398ae39f6644b69c78938e5603210f966
alex <alexsawyer95@hotmail.com>
2026-03-22 15:13:10 +0000

    Test coverage

--- src/GlucoPilot.Nutrition.Tests/Endpoints/SearchProduct/SearchProductEndpointTests.cs
+++ src/GlucoPilot.Nutrition.Tests/Endpoints/SearchProduct/SearchProductEndpointTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using GlucoPilot.AspNetCore.Exceptions;
 using GlucoPilot.Data.Entities;
 using GlucoPilot.Data.Tests;
 using GlucoPilot.Identity.Authentication;
@@ -33,6 +34,15 @@ public class SearchProductEndpointTests
         _currentUserMock = new Mock<ICurrentUser>();
         
         _currentUserMock.Setup(c => c.GetUserId()).Returns(_userId);
+    }
+    
+    [Test]
+    public async Task HandleAsync_Returns_Unauthorized_When_User_Is_Not_Authenticated()
+    {
+        _currentUserMock.Setup(x => x.GetUserId()).Throws(new UnauthorizedException("USER_NOT_LOGGED_IN")); ;
+
+        Assert.That(() => Endpoint.HandleAsync("search", null, _repoMock.Object, _gpRepoMock.Object, _currentUserMock.Object, CancellationToken.None),
+            Throws.InstanceOf<UnauthorizedException>().With.Message.EqualTo("USER_NOT_LOGGED_IN"));
     }
     
     [Test]
@@ -113,6 +123,89 @@ public class SearchProductEndpointTests
         var okResult = result.Result as Ok<IEnumerable<ProductResponse>>;
         var actual = okResult!.Value!.ToList();
         Assert.That(actual, Has.Count.EqualTo(10));
+    }
+
+    [Test]
+    public async Task HandleAsync_Returns_Correct_ProductResponse_For_Product()
+    {
+        var product = new Product
+        {
+            Id = "p1",
+            ProductType = "Food",
+            Quantity = "1",
+            ProductQuantityUnit = "g",
+            ProductName = "Test Product",
+            ProductQuantity = 123.45,
+            NutritionDataPer = "100g",
+            NutritionDataPreparedPer = "prepared",
+            Code = "ABC123",
+            ServingQuantity = 50.5,
+            Nutriments = new Nutriments
+            {
+                EnergyUnit = "kJ",
+                FatUnit = "g",
+                CarbohydratesUnit = "g",
+                EnergyKcalUnit = "kcal",
+                EnergyKcalValue = 200.1,
+                EnergyValue = 800.2,
+                CarbohydratesValue = 30.3,
+                Proteins = 10.4f,
+                EnergyKcalValueComputed = 201.5,
+                ProteinsValue = 11.6f,
+                EnergyKcal = 202.7,
+                ProteinsUnit = "g",
+                Carbohydrates = 31.8,
+                Energy = 900.9f,
+                Fat = 5.5f,
+                FatValue = 6.6f
+            }
+        };
+
+        _repoMock.Setup(r => r.Find(
+                It.IsAny<System.Linq.Expressions.Expression<System.Func<Product, bool>>>(),
+                It.IsAny<FindOptions>()))
+            .Returns(new TestAsyncEnumerable<Product>(new[] { product }));
+        _gpRepoMock.Setup(r => r.Find(
+                It.IsAny<System.Linq.Expressions.Expression<System.Func<Ingredient, bool>>>(),
+                It.IsAny<GPRepository.FindOptions>()))
+            .Returns(new TestAsyncEnumerable<Ingredient>(GenerateIngredients(0)));
+        
+        var result = await Endpoint.HandleAsync("Test Product", 1, _repoMock.Object, _gpRepoMock.Object, _currentUserMock.Object, CancellationToken.None);
+        
+        Assert.That(result.Result, Is.TypeOf<Ok<IEnumerable<ProductResponse>>>());
+        var okResult = result.Result as Ok<IEnumerable<ProductResponse>>;
+        var actual = okResult!.Value!.Single();
+        Assert.Multiple(() =>
+        {
+            Assert.That(actual.Id, Is.EqualTo(product.Id));
+            Assert.That(actual.ProductType, Is.EqualTo(product.ProductType));
+            Assert.That(actual.Quantity, Is.EqualTo(product.Quantity));
+            Assert.That(actual.ProductQuantityUnit, Is.EqualTo(product.ProductQuantityUnit));
+            Assert.That(actual.ProductName, Is.EqualTo(product.ProductName));
+            Assert.That(actual.ProductQuantity, Is.EqualTo(product.ProductQuantity));
+            Assert.That(actual.NutritionDataPer, Is.EqualTo(product.NutritionDataPer));
+            Assert.That(actual.NutritionDataPreparedPer, Is.EqualTo(product.NutritionDataPreparedPer));
+            Assert.That(actual.Code, Is.EqualTo(product.Code));
+            Assert.That(actual.ServingQuantity, Is.EqualTo(product.ServingQuantity));
+            var n = actual.Nutriments;
+            Assert.That(n, Is.Not.Null);
+            Assert.That(n!.EnergyUnit, Is.EqualTo(product.Nutriments.EnergyUnit));
+            Assert.That(n.FatUnit, Is.EqualTo(product.Nutriments.FatUnit));
+            Assert.That(n.CarbohydratesUnit, Is.EqualTo(product.Nutriments.CarbohydratesUnit));
+            Assert.That(n.EnergyKcalUnit, Is.EqualTo(product.Nutriments.EnergyKcalUnit));
+            Assert.That(n.EnergyKcalValue, Is.EqualTo(product.Nutriments.EnergyKcalValue));
+            Assert.That(n.EnergyValue, Is.EqualTo(product.Nutriments.EnergyValue));
+            Assert.That(n.CarbohydratesValue, Is.EqualTo(product.Nutriments.CarbohydratesValue));
+            Assert.That(n.Proteins, Is.EqualTo(product.Nutriments.Proteins));
+            Assert.That(n.EnergyKcalValueComputed, Is.EqualTo(product.Nutriments.EnergyKcalValueComputed));
+            Assert.That(n.ProteinsValue, Is.EqualTo(product.Nutriments.ProteinsValue));
+            Assert.That(n.EnergyKcal, Is.EqualTo(product.Nutriments.EnergyKcal));
+            Assert.That(n.ProteinsUnit, Is.EqualTo(product.Nutriments.ProteinsUnit));
+            Assert.That(n.Carbohydrates, Is.EqualTo(product.Nutriments.Carbohydrates));
+            Assert.That(n.Energy, Is.EqualTo(product.Nutriments.Energy));
+            Assert.That(n.Fat, Is.EqualTo(product.Nutriments.Fat));
+            Assert.That(n.FatValue, Is.EqualTo(product.Nutriments.FatValue));
+        });
     }
 
     private static IEnumerable<Product> GenerateProducts(int count = 100)
