aaa757a2131259d7bde680abd83171cf330adb50
Joseph Sawyer <joseph@jcsawyer.com>
2026-04-11 13:24:59 +0100

    add patient region migration

---
+++ src/GlucoPilot.Data.Migrators.MSSQL/Migrations/20260411122436_PatientRegion.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GlucoPilot.Data.Migrators.MSSQL.Migrations
+{
+    /// <inheritdoc />
+    public partial class PatientRegion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Region",
+                table: "users",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Region",
+                table: "users");
+        }
+    }
+}
